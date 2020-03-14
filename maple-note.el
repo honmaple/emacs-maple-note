8b84f1a01c61ccf2b46e1550540012962bd40785
honmaple <xiyang0807@gmail.com>
2020-03-14 16:34:18 +0800

    update

--- maple-note.el
+++ maple-note.el
@@ -49,124 +49,161 @@
 (defcustom maple-note-sort-key '("DATE" . t)
   "Note sort key."
   :group 'maple-note
-  :type 'list)
+  :type '(alist :key-type (string) :value-type (boolean)))
 
 (defcustom maple-note-filter-keyword nil
   "Global filter keywords."
   :group 'maple-note
   :type 'string)
 
-(defcustom maple-note-root-path nil
-  "Note root path."
-  :group 'maple-note
-  :type 'string)
-
-(defcustom maple-note-draft-path nil
-  "Note draft path."
-  :group 'maple-note
-  :type 'string)
-
-(defcustom maple-note-path nil
-  "Note path."
+(defcustom maple-note-base-directory nil
+  "Note base directory."
   :group 'maple-note
   :type 'list)
 
-(defcustom maple-note-file "^[^.]*\\.\\(org\\|md\\|markdown\\)$"
+(defcustom maple-note-draft-directory "draft/"
+  "Note draft directory."
+  :group 'maple-note
+  :type 'list)
+
+(defcustom maple-note-publishing-directory
+  '((org-mode . "org/")
+    (markdow-mode . "markdown/"))
+  "Note publishing directory."
+  :group 'maple-note
+  :type '(alist :key-type (symbol) :value-type (string)))
+
+(defcustom maple-note-file-match "^[^.]*\\.\\(org\\|md\\|markdown\\)$"
   "Note scan file."
   :group 'maple-note
   :type 'string)
 
-(defcustom maple-note-alist
-  '(("markdown"
-     :path "content/markdown"
-     :read maple-note--read-md
-     :template "Title: %s
-Author: honmaple
-Date: %s
-Modified: %s
-Category:
-Tags:
-Slug: %s
-Summary: ")
-    ("org-mode"
-     :path "content/org"
-     :read maple-note--read-org
-     :template "#+TITLE: %s
-#+AUTHOR: honmaple
-#+DATE: %s
-#+CATEGORY:
-#+PROPERTY: MODIFIED %s
-#+PROPERTY: TAGS
-#+PROPERTY: SLUG %s
-#+PROPERTY: SUMMARY "))
+(defvar maple-note-md-template
+  '("Title: %s"
+    "Author: honmaple"
+    "Date: %s"
+    "Modified: %s"
+    "Category:"
+    "Tags:"
+    "Slug: %s"
+    "Summary: "))
+
+(defvar maple-note-org-template
+  '("#+TITLE: %s"
+    "#+AUTHOR: honmaple"
+    "#+DATE: %s"
+    "#+CATEGORY:"
+    "#+PROPERTY: MODIFIED %s"
+    "#+PROPERTY: TAGS"
+    "#+PROPERTY: SLUG %s"
+    "#+PROPERTY: SUMMARY "))
+
+(defcustom maple-note-template-alist
+  `((org-mode . ,maple-note-org-template)
+    (markdown-mode . ,maple-note-md-template))
   "Note template list."
   :group 'maple-note
-  :type '(list))
+  :type '(alist :key-type (symbol) :value-type (list)))
+
+(defvar maple-note-md-meta
+  '(:category
+    "^Category:\\(.*\\)$"
+    :title "^Title:\\(.*\\)$"
+    :tags "^Tags:\\(.*\\)$"
+    :date "^Date:\\(.*\\)$"))
+
+(defvar maple-note-org-meta
+  '(:category
+    "^#\\+CATEGORY:\\(.*\\)$"
+    :title "^#\\+TITLE:\\(.*\\)$"
+    :tags "^#\\+PROPERTY: TAGS\\(.*\\)$"
+    :date "^#\\+DATE:\\(.*\\)$"))
+
+(defcustom maple-note-meta-alist
+  `((org-mode . ,maple-note-org-meta)
+    (markdown-mode . ,maple-note-md-meta))
+  "Note meta list."
+  :group 'maple-note
+  :type '(alist :key-type (symbol) :value-type (list)))
+
+(defcustom maple-note-read-alist nil
+  "Note scan file."
+  :group 'maple-note
+  :type '(alist :key-type (symbol) :value-type (symbol)))
+
+(defcustom maple-note-draft-alist
+  '((t . maple-note-draft))
+  "Note scan file."
+  :group 'maple-note
+  :type '(alist :key-type (symbol) :value-type (symbol)))
 
 (defface maple-note-face
   '((t (:underline nil :inherit font-lock-keyword-face)))
   "Note text face.")
 
-(defmacro maple-note--with-buffer (&rest body)
+(defmacro maple-note-with (&rest body)
   "Execute the forms in BODY with maple note buffer."
   (declare (indent 0) (debug t))
-  `(let ((buffer (get-buffer-create maple-note-buffer-name)))
-     (with-current-buffer buffer
-       ,@body)))
+  `(with-current-buffer (get-buffer-create maple-note-buffer-name) ,@body))
 
-(defun maple-note--read-md (file)
-  "Read info of markdown FILE."
+(defun maple-note-alist (file alist)
+  "FILE ALIST."
+  (let ((key (assoc-default file auto-mode-alist 'string-match)))
+    (when key (cdr (or (and key (assq key alist)) (assq t alist))))))
+
+(defun maple-note-abspath (path)
+  "Return full absolute path base on PATH."
+  (if (file-name-absolute-p path) path
+    (expand-file-name path maple-note-base-directory)))
+
+(defun maple-note-draft (file)
+  "Check FILE is in drafts."
+  (string-prefix-p
+   (maple-note-abspath maple-note-draft-directory) file))
+
+(defun maple-note--match(regex string)
+  "Match REGEX STRING."
+  (let ((match (s-match regex string)))
+    (if match (s-trim (cadr match)) "")))
+
+(defun maple-note--date (datestring &optional fstring)
+  "Format DATESTRING FSTRING."
+  (let ((date (apply 'encode-time (cl-loop for item in (parse-time-string (or datestring ""))
+                                           for i from 1 to 6
+                                           collect (or item 0)))))
+    (if fstring (format-time-string fstring date) date)))
+
+(defun maple-note--read (file)
+  "Read FILE metadata."
   (with-temp-buffer
     (insert-file-contents file)
-    (let ((title (s-chop-prefix "Title:" (car (s-match "^Title:.*?\n" (buffer-string)))))
-          (category (s-chop-prefix "Category: " (car (s-match "^Category:.*?\n" (buffer-string)))))
-          (tags (s-chop-prefix "Tags:" (car (s-match "^Tags:.*?\n" (buffer-string)))))
-          (date (s-chop-prefix "Date:" (car (s-match "^Date:.*?\n" (buffer-string))))))
-      (list :title (s-trim title)
-            :category (s-trim category)
-            :tags (s-trim tags)
-            :date (maple-note--format-datetime (s-trim date))))))
+    (let ((content (buffer-string))
+          (draft (maple-note-alist file maple-note-draft-alist)))
+      (cl-destructuring-bind (&key category title tags date)
+          (maple-note-alist file maple-note-meta-alist)
+        (list :category (maple-note--match category content)
+              :title (maple-note--match title content)
+              :draft (when draft (funcall draft file))
+              :tags  (maple-note--match tags content)
+              :date  (maple-note--date (maple-note--match date content))
+              :file  file)))))
 
-(defun maple-note--read-org (file)
-  "Read info of org FILE."
-  (with-temp-buffer
-    (insert-file-contents file)
-    (let ((title (s-chop-prefix "#+TITLE:" (car (s-match "^#\\+TITLE:.*?\n" (buffer-string)))))
-          (category (s-chop-prefix "#+CATEGORY:" (car (s-match "^#\\+CATEGORY:.*?\n" (buffer-string)))))
-          (tags (s-chop-prefix "#+PROPERTY: TAGS" (car (s-match "^#\\+PROPERTY: TAGS.*?\n" (buffer-string)))))
-          (date (s-chop-prefix "#+DATE:" (car (s-match "^#\\+DATE:.*?\n" (buffer-string))))))
-      (list :title (s-trim title)
-            :category (s-trim category)
-            :tags (s-trim tags)
-            :date (maple-note--format-datetime (s-trim date))))))
+(defun maple-note-read (file)
+  "Read info of pelican FILE."
+  (let ((read (maple-note-alist file maple-note-read-alist)))
+    (if read (funcall read file)
+      (maple-note--read file))))
 
-(defun maple-note--read (filename)
-  "Read info of pelican FILENAME."
-  (let* ((args (maple-note--info filename))
-         (info (funcall (plist-get args :read) filename)))
-    (if (maple-note--is-draft filename)
-        (plist-put info :publish "NO")
-      (plist-put info :publish "YES"))))
-
-(defun maple-note--info(file)
-  "Check FILE path."
-  (let ((e (file-name-extension file)))
-    (cond ((string= e "md")
-           (cdr (assoc "markdown" maple-note-alist)))
-          ((string= e "org")
-           (cdr (assoc "org-mode" maple-note-alist)))
-          (t nil))))
-
-(defun maple-note--entry (file)
-  "Get info of FILE."
-  (let* ((info (maple-note--read file))
-         (title (plist-get info :title))
-         (publish (plist-get info :publish))
-         (category (plist-get info :category))
+(defun maple-note-entries (file)
+  "Get entries of FILE."
+  (let* ((info (maple-note-read file))
+         (date (plist-get info :date))
          (tags (plist-get info :tags))
-         (date (plist-get info :date)))
+         (title (plist-get info :title))
+         (draft (plist-get info :draft))
+         (category (plist-get info :category)))
     (list file (vector
-                `(,date
+                `(,(format-time-string "%Y-%m-%d %H:%M" date)
                   face font-lock-comment-face
                   follow-link t
                   action (lambda(&optional _) (find-file (tabulated-list-get-id))))
@@ -174,124 +211,85 @@ Summary: ")
                   face maple-note-face
                   follow-link t
                   action (lambda(&optional _) (find-file (tabulated-list-get-id))))
-                `(,publish
-                  face ,(if (string= publish "YES")
-                            'font-lock-comment-face
-                          'maple-note-face)
+                `(,(if draft "YES" "NO")
+                  face ,(if draft 'maple-note-face 'font-lock-comment-face)
                   action (lambda(&optional _) (maple-note-publish-or-unpublish)))
                 `(,category
                   face maple-note-face
                   action (lambda(&optional _) (maple-note-refresh ,category)))
-                `(,tags
-                  face maple-note-face)))))
+                `(,tags face maple-note-face)))))
 
-(defun maple-note--files (&optional path)
-  "Scan files of PATH."
-  (apply 'append
-         (mapcar
-          (lambda (append-path)
-            "scan files with append-path"
-            (directory-files (maple-note--abspath append-path) t maple-note-file))
-          (or path (append maple-note-path (list maple-note-draft-path))))))
-
-(defun maple-note--format-datetime (datetime)
-  "Format DATETIME."
-  (let* ((datetime-in-plist
-          (when (not (stringp datetime))
-            (plist-get (plist-get (car datetime) 'timestamp) :raw-value)))
-         (datetime-str (cond
-                        ((eq datetime nil) "") ;; nil
-                        ((stringp datetime-in-plist) datetime-in-plist)
-                        ((stringp datetime) datetime)
-                        (t (car datetime))))
-         (l (parse-time-string datetime-str)))
-    (if (equal l '(nil nil nil nil nil nil nil nil nil)) ;; can't parse
-        "Can't Parse"
-      (format-time-string "%Y-%m-%d %H:%M"
-                          (encode-time 0 (or (nth 1 l) 0) (or (nth 2 l) 0) (nth 3 l) (nth 4 l) (nth 5 l))))))
-
-(defun maple-note--is-draft (path)
-  "Check PATH is in drafts."
-  (string-prefix-p
-   (maple-note--abspath maple-note-draft-path) path))
-
-(defun maple-note--abspath (path)
-  "Return full absolute path base on PATH."
-  (if (file-name-absolute-p path) path
-    (expand-file-name path (file-name-as-directory maple-note-root-path))))
+(defun maple-note-file-list ()
+  "Scan all files."
+  (cl-loop
+   for dir in (append (list (maple-note-abspath maple-note-draft-directory))
+                      (cl-loop for dir in maple-note-publishing-directory
+                               collect (maple-note-abspath (cdr dir))))
+   append (directory-files (maple-note-abspath dir) t maple-note-file-match)))
 
 (defun maple-note-publish-or-unpublish ()
   "Switch between publish and draft."
   (interactive)
   (let* ((point (point))
-         (path (tabulated-list-get-id))
-         (info (maple-note--info path))
-         (dest-path (if (maple-note--is-draft path)
-                        (maple-note--abspath (plist-get info :path))
-                      maple-note-draft-path)))
-    (when (file-exists-p path)
-      (rename-file path (expand-file-name
-                         (file-name-nondirectory path)
-                         (maple-note--abspath dest-path)))
-      (let ((buffer (find-buffer-visiting path)))
+         (file  (tabulated-list-get-id))
+         (draft (maple-note-alist file maple-note-draft-alist))
+         (dest-path (if (funcall draft file)
+                        (maple-note-alist file maple-note-publishing-directory)
+                      (maple-note-abspath maple-note-draft-directory))))
+    (when (file-exists-p file)
+      (rename-file file (expand-file-name
+                         (file-name-nondirectory file)
+                         (maple-note-abspath dest-path)))
+      (let ((buffer (find-buffer-visiting file)))
         (when buffer (kill-buffer buffer))))
     (maple-note-refresh) (goto-char point)))
 
 (defun maple-note-create (filename)
   "New note with FILENAME."
   (interactive "sNote's filename(new-note.org, new-note.md etc):")
-  (let ((info (maple-note--info filename))
+  (let ((temp (maple-note-alist filename maple-note-template-alist))
         (time (format-time-string "%F %T" (current-time))))
-    (if (not info) (message "Unsupported file extension!")
-      (find-file (expand-file-name filename (maple-note--abspath maple-note-draft-path)))
-      (insert (format (plist-get info :template)
-                      (file-name-base filename) time time
-                      (file-name-base filename))))))
+    (if (not (string-match maple-note-file-match filename))
+        (message "Unsupported file extension!")
+      (find-file (expand-file-name filename (maple-note-abspath maple-note-draft-directory)))
+      (when temp (insert (format (string-join temp "\n") (file-name-base filename) time time (file-name-base filename)))))))
 
 (defun maple-note-delete ()
   "Delete note."
   (interactive)
-  (let ((file-path (tabulated-list-get-id)))
-    (when (y-or-n-p (format "Do you really want to delete %s ? " file-path))
-      (delete-file file-path t)
-      (let ((buffer (find-buffer-visiting file-path)))
+  (let ((file (tabulated-list-get-id)))
+    (when (y-or-n-p (format "Do you really want to delete %s ? " file))
+      (delete-file file t)
+      (let ((buffer (find-buffer-visiting file)))
         (when buffer (kill-buffer buffer)))
       ;; remove asset directory if exist
-      (let ((dir-path (file-name-sans-extension file-path)))
-        (if (file-exists-p dir-path)
-            (delete-directory dir-path t)))
+      ;; (let ((dir-path (file-name-sans-extension file)))
+      ;;   (if (file-exists-p dir-path)
+      ;;       (delete-directory dir-path t)))
       (maple-note-refresh))))
 
 (defun maple-note-refresh(&optional keyword remember-pos update)
   "Refresh with &optional KEYWORD REMEMBER-POS UPDATE."
   (interactive)
-  (maple-note--with-buffer
-   (let ((notes (mapcar 'maple-note--entry (maple-note--files)))
-         (filter-keyword (or keyword maple-note-filter-keyword)))
-     (when filter-keyword
-       (setq notes (cl-remove-if-not
-                    (lambda (x)
-                      (let* ((row (append (car (cdr x)) nil))
-                             (title (car (nth 1 row)))
-                             (category (car (nth 3 row))))
-                        (s-contains? keyword (mapconcat #'identity (list title category) "|") t)))
-                    notes)))
-     (setq tabulated-list-sort-key maple-note-sort-key)
-     (setq tabulated-list-entries notes)
-     (tabulated-list-print remember-pos update))))
+  (maple-note-with
+    (let ((notes (mapcar 'maple-note-entries (maple-note-file-list)))
+          (filter-keyword (or keyword maple-note-filter-keyword)))
+      (when filter-keyword
+        (setq notes (cl-remove-if-not
+                     (lambda (x)
+                       (let* ((row (append (car (cdr x)) nil))
+                              (title (car (nth 1 row)))
+                              (category (car (nth 3 row))))
+                         (s-contains? keyword (mapconcat #'identity (list title category) "|") t)))
+                     notes)))
+      (setq tabulated-list-sort-key maple-note-sort-key)
+      (setq tabulated-list-entries notes)
+      (tabulated-list-print remember-pos update))))
 
 (defun maple-note-filter()
   "Filter notes."
   (interactive)
   (maple-note-refresh (read-from-minibuffer "Search filter: ")))
-
-(defun maple-note-init()
-  "Init notes."
-  (maple-note--with-buffer
-   (dolist (arg maple-note-alist)
-     (add-to-list 'maple-note-path (plist-get (cdr arg) :path)))
-   (setq tabulated-list-format maple-note-header)
-   (tabulated-list-init-header)))
 
 (defvar maple-note-mode-map
   (let ((map (make-sparse-keymap))
@@ -308,24 +306,23 @@ Summary: ")
                   ("I" . beginning-of-line)
                   ("gg" . beginning-of-buffer))))
     (set-keymap-parent map tabulated-list-mode-map)
-    (dolist (key keymap)
-      (define-key map (car key) (cdr key))
-      (define-key map (concat "?" (car key)) (cdr key)))
-    map)
+    (dolist (key keymap) (define-key map (car key) (cdr key))) map)
   "Maple note mode map.")
 
 (define-derived-mode maple-note-mode tabulated-list-mode "NOTE"
   "Major mode for writing note.
 \\<maple-note-mode-map>
 \\{maple-note-mode-map}"
-  (maple-note-init)
+  (maple-note-with
+    (setq tabulated-list-format maple-note-header)
+    (tabulated-list-init-header))
   (maple-note-refresh))
 
 ;;;###autoload
 (defun maple-note()
   "Start note."
   (interactive)
-  (maple-note--with-buffer (maple-note-mode))
+  (maple-note-with (maple-note-mode))
   (switch-to-buffer maple-note-buffer-name))
 
 (provide 'maple-note)
